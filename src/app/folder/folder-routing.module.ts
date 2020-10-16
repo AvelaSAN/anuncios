6d3e2f5a114a6054484269ece8dbfee9184b7c85
AvelaSAN <avela.sanchez123@gmail.com>
2020-10-15 21:50:04 -0500

    Se agrega las fuentes
    inicio

---
+++ src/app/folder/folder-routing.module.ts
@@ -0,0 +1,60 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+
+import { FolderPage } from './folder.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: FolderPage,
+      children: [
+        {
+          path: 'account',
+          children: [
+            {
+              path: '',
+              loadChildren: () => import('../pages/account/account.module').then(m => m.AccountPageModule)
+            }
+          ]
+        },
+        {
+          path: 'calls',
+          children: [
+            {
+              path: '',
+              loadChildren: () => import('../pages/calls/calls.module').then(m => m.CallsPageModule)
+            }
+          ]
+        },
+        {
+          path: 'inbox',
+          children: [
+            {
+              path: '',
+              loadChildren: () => import('../pages/inbox/inbox.module').then(m => m.InboxPageModule)
+            }
+          ]
+        },
+        {
+          path: '',
+          redirectTo: '/tabs/account',
+          pathMatch: 'full'
+        }
+      ]
+    },
+    {
+      path: 'account',
+      children: [
+        {
+          path: '',
+          loadChildren: () => import('../pages/account/account.module').then(m => m.AccountPageModule)
+        }
+      ]
+    }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class FolderPageRoutingModule {}
